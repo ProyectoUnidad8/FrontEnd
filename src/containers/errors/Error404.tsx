a80a94eba4f7a344d685a951029d5ba2bb6b9150
FreddyxD5 <freddyxd5@gmail.com>
2023-02-04 09:33:53 -0500

    Añadido HOC Layout(Navbar/Footer)/ añadido Formulario Login/ Añadido pagina de erroes

---
+++ src/containers/errors/Error404.tsx
@@ -0,0 +1,17 @@
+import Layout from "../../hocs/layout";
+const Error = () => {
+return(
+    <Layout>
+        <>
+        <section id="about" className="pages">
+            <div className="jumbotron" data-stellar-background-ratio="0.5">                    
+                <div className="jumbo-heading" data-stellar-background-ratio="1.2">
+                    <h1>Te has perdido? :( </h1>
+                </div>
+            </div>               
+        </section>
+        </>
+    </Layout>
+)
+};
+export default Error;
