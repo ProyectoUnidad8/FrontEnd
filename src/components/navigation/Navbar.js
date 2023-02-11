fc3b97459f6d04a6686c81a2ac7aa966db71fc3a
everyzzz <jquisper2@gmail.com>
2023-02-11 01:52:12 -0500

    edit bug in nav

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,60 +1,75 @@
-import { Fragment,useEffect,useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import logo from "../../assets/img/logo.png"
 import { logOut } from "../../utils/Auth";
 import { lastToken } from "../../utils/LocalStorage";
 
-function Navbar() {  
+
+function Navbar() {
    const [isAuthenticated, setIsAuthenticated] = useState(false)
-   
-   const guestLinks = (
-      <Fragment>                  
+
+   const LoginLink = (
+      <Fragment>
          <li><a href="/login">Login</a></li>
       </Fragment>
    )
-  
+   const guestLinks = (
+      <Fragment>
+         <li><a href="/">Home</a></li>
+         <li><a href="/adopt-pet">¡Adopta!</a></li>
+      </Fragment>
+   )
 
-   const checkRole = (role) =>{      
-         if (role==='ADMIN'){
-            return (
-               <Fragment>                  
-                  <li><a href="/admin/usuarios">Clientes</a></li>
-                  <li><a href="/admin/mascota">Mascotas</a></li>
-                  <li><a href="/admin/aplications">Solicitudes</a></li>
-                  <li><a href="/admin/diagnosticos">Diagnosticos Totales</a></li>
-                  <li><a href="/admin/adoptions">Mascotas Adopcion</a></li>   
-                  <li><a href="/admin/donaciones">Donaciones</a></li>   
-                  <li><a href="/" onClick={logOut}>Logout</a></li>
-         </Fragment>)
-         }else if (role==='USER') {
-            return (
-               <Fragment>      
-               <li><a href="/mis_mascotas">Mis Mascotas</a></li>         
-               <li><a href="/" onClick={logOut}>Logout</a></li>         
+
+   const checkRole = (role) => {
+      if (role === 'ADMIN') {
+         return (
+            <Fragment>
+               <nav className="navbar navbar-custom navbar-fixed-top" style={{ zIndex: "10" }}>
+                  <div className="container">
+                     <div className="collapse navbar-collapse" id="navbar-brand-centered">
+                        <ul className="adminnav">
+                           <li><a href="/admin/usuarios">Clientes</a></li>
+                           <li><a href="/admin/mascota">Mascotas</a></li>
+                           <li><a href="/admin/diagnosticos">Diagnósticos</a></li>
+
+                           <img src={logo} onClick={() => window.location.href = "/"} id="image-nav-logo" alt="" />
+
+                           <li><a href="/admin/aplications">Solicitudes</a></li>
+                           <li><a href="/admin/adoptions" >Adopción</a></li>
+                           <li><a href="/admin/donaciones" >Donaciones</a></li>
+                           <li><a href="/" onClick={logOut}>Logout</a></li>
+                        </ul>
+                     </div>
+                  </div>
+               </nav>
+            </Fragment>)
+      } else if (role === 'USER') {
+         return (
+            <Fragment>
+               <li><a href="/mis_mascotas">Mis Mascotas</a></li>
+               <li><a href="/" onClick={logOut}>Logout</a></li>
             </Fragment>
-            )
-         }  
+         )
+      }
    }
 
 
-   useEffect(()=>{
-      if(lastToken){
-         setIsAuthenticated(true)         
-      }else{
+   useEffect(() => {
+      if (lastToken) {
+         setIsAuthenticated(true)
+      } else {
          setIsAuthenticated(false)
       }
-   },[])
-   
+   }, [])
+
    return (
       <Fragment>
-         <link href="https://fonts.googleapis.com/css?family=Lato:400,800" rel="stylesheet"/>
-         <link href="https://fonts.googleapis.com/css?family=Baloo+Thambi" rel="stylesheet"/>
-         <nav className="navbar navbar-custom navbar-fixed-top" style={{ zIndex: "10"}}>
+         <link href="https://fonts.googleapis.com/css?family=Lato:400,800" rel="stylesheet" />
+         <link href="https://fonts.googleapis.com/css?family=Baloo+Thambi" rel="stylesheet" />
+         <nav className="navbar navbar-custom navbar-fixed-top" style={{ zIndex: "10" }}>
             <div className="container">
                {/* <!-- Brand and toggle get grouped for better mobile display --> */}
                <div className="navbar-header">
-                  <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
-                     <i className="fa fa-bars"></i>
-                  </button>
                   <div className="navbar-brand navbar-brand-centered page-scroll">
                      <a href="/">
                         {/* <!-- logo  --> */}
@@ -66,19 +81,17 @@ function Navbar() {
                {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
                <div className="collapse navbar-collapse" id="navbar-brand-centered">
                   <ul className="nav navbar-nav">
-                     <li><a href="/">Home</a></li>                     
-                     <li><a href="/adopt-pet">¡Adopta!</a></li>
+                     {isAuthenticated ? localStorage.getItem('role') === 'USER' ? guestLinks : checkRole(localStorage.getItem('role')) : guestLinks}
                   </ul>
-                  <ul className="nav navbar-nav navbar-right">                     
-                     {isAuthenticated? checkRole(localStorage.getItem('role')) : guestLinks}
+                  <ul className="nav navbar-nav navbar-right">
+                     {isAuthenticated ? checkRole(localStorage.getItem('role')) : LoginLink}
                   </ul>
                </div>
                {/* <!-- /.navbar-collapse --> */}
             </div>
             {/* <!-- /.container --> */}
-         </nav>         
+         </nav>
       </Fragment>
-
    )
 }
 
--- src/containers/auth/Login.tsx
+++ src/containers/auth/Login.tsx
@@ -2,153 +2,37 @@ import Layout from "../../hocs/layout";
 import { LoginFunction } from "../../functions/AuthLogin";
 
 const LoginForm = () => {
-    return (        
+    return (
         <Layout>
-            <section className="container">
-                <div className="row margin1">
-                    <div className="col-lg-5 col-lg-offset-1 col-md-12">                        
-                        <h5 className="sub-header">Iniciar Sesión</h5>        
-                        <form method='post' onSubmit={ LoginFunction } id="contact_form" autoComplete="off" >
-                            <div className="form-group">
-                                <label>email<span className="required">*</span></label>
-                                <input 
-                                    name="email"
-                                    type="text"                                    
-                                    autoComplete="off"
-                                    className="form-control input-field" 
-                                    placeholder="Ingrese su email"
-                                    required={true}/>                    
-                                <label>Contraseña <span className="required">*</span></label>
-                                <input 
-                                    type="password"
-                                    name="password"
-                                    className="form-control input-field"
-                                    autoComplete="off"
-                                    placeholder="Ingrese su contraseña"
-                                    required={true} />                                       
-                            </div>
-                                <button type="submit" id="submit_btn" value="Submit" className="btn center-block">Iniciar Sesión</button>
-                        </form>                        
-                            <div id="contact_results"></div>
-                    </div> 
+            <div style={{ marginTop: "150px", marginBottom: "100px", display: "flex", alignContent: "center", alignItems: "center", justifyContent: "center" }}>
+                <div style={{ width: "50%" }}>
+                    <h5 className="sub-header">Iniciar Sesión</h5>
+                    <form method='post' onSubmit={LoginFunction} id="contact_form" autoComplete="off">
+                        <div className="form-group">
+                            <label>Email<span className="required">*</span></label>
+                            <input
+                                name="email"
+                                type="text"
+                                autoComplete="off"
+                                className="form-control input-field"
+                                placeholder="Ingrese su email"
+                                required={true} />
+                            <label>Contraseña <span className="required">*</span></label>
+                            <input
+                                type="password"
+                                name="password"
+                                className="form-control input-field"
+                                autoComplete="off"
+                                placeholder="Ingrese su contraseña"
+                                required={true} />
+                        </div>
+                        <button type="submit" id="submit_btn" value="Submit" className="btn center-block">Iniciar Sesión</button>
+                    </form>
+                    <div id="contact_results"></div>
                 </div>
-            </section>                         
+            </div>
         </Layout>
     )
 }
 
 export default LoginForm;
-
-
-
-
-// const Login= ({
-
-// }) => {        
-//     const [formData, setFormData] = useState({
-//         email:'',
-//         password:''
-//     })
-
-//     const { 
-//         email,
-//         password
-//     } = formData;
-
-//     const captchaRef = useRef(null);
-
-//     const onChange = e => setFormData({...formData,[e.target.name]:e.target.value});
-//     useEffect(()=>{
-//         console.log("Datos del formulario")
-//         console.log(formData);
-//     })
-
-//     const verifyToken = async (token) => {
-//         try{
-//             let response = await axios.post(`http://localhost:4000/verify-token`,{
-//                 secret:process.env.REACT_APP_SECRET_KEY,
-//                 token
-//             }, console.log(token))
-//             return response.data;
-            
-//         }catch(error){
-//             console.log("error",error)
-//         }
-//     }
-//     const onSubmit = async e => {
-//         e.preventDefault();
-//         let token = captchaRef.current.getValue();
-
-//         if (email && password){
-//             if(token){
-//                 let valid_token = await verifyToken(token);
-//                 if (valid_token.success){
-//                     console.log("Congrats has emitido el formulario");
-//                 }else{
-//                     console.log("Nope invalid token")
-//                 }
-    
-//             }else{
-//                 console.log("debes confirmar que no eres un robot")
-//             }
-//         }else{
-//             console.log("Debes ingresar los datos.")
-//         }
-        
-//     }
-//     // const onSubmit = e => {
-//     //     e.preventDefault();
-//     //     console.log(formData);
-
-//     // }
-
-
-    
-//     return (        
-//         <Layout>
-//             <section className="container">
-//                 <div className="row margin1">
-//                     <div className="col-lg-5 col-lg-offset-1 col-md-12">                        
-//                         <h5 className="sub-header">Iniciar session</h5>        
-//                         <form onSubmit={e=>onSubmit(e)} id="contact_form" autoComplete="off">
-//                             <div className="form-group">
-//                                 <label>email<span className="required">*</span></label>
-//                                 <input 
-//                                     name="email"
-//                                     type="email"                                    
-//                                     value={email}
-//                                     autoComplete="off"
-//                                     onChange={e=>onChange(e)}
-//                                     className="form-control input-field" 
-//                                     placeholder="Ingrese su email"
-//                                     required/>                    
-//                                 <label>Contraseña <span className="required">*</span></label>
-//                                 <input 
-//                                     type="password"
-//                                     name="password"
-//                                     value={password}
-//                                     onChange={e=>onChange(e)}
-//                                     className="form-control input-field"
-//                                     autoComplete="off"
-//                                     placeholder="Ingrese su contraseña"
-//                                     required=""/>                                       
-//                             </div>
-                            
-//                             <div className="form-group">                                
-//                                 <ReCAPTCHA 
-//                                 sitekey={process.env.REACT_APP_PUBLIC_KEY}
-//                                 ref={captchaRef}
-//                                 />
-//                             </div>
-//                             <button type="submit" id="submit_btn" value="Submit" className="btn center-block">Iniciar Session</button>
-//                         </form>                        
-//                         <div id="contact_results"></div>
-
-
-//                     </div> 
-//                 </div>
-//             </section>                         
-//         </Layout>
-// )}
-
-//export default Login;
